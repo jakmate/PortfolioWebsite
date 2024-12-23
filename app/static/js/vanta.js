997a011cc29f0cae5ea081b1aa8a9f4c69bb9995
jakmate <jakub.orzolek04@gmail.com>
2024-12-23 22:14:51 +0000

    UI overhaul

--- app/static/js/vanta.js
+++ app/static/js/vanta.js
@@ -8,8 +8,8 @@ document.addEventListener("DOMContentLoaded", function() {
         minWidth: 200.00,
         scale: 1.00,
         scaleMobile: 1.00,
-        color: 0x898e42,
-        backgroundColor: 0x2d143c,
+        color: 0xadbbda,
+        backgroundColor: 0x3d52a0,
         points: 12.00,
         maxDistance: 20.00
     });
