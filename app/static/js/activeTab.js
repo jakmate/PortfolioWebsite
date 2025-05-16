958d533508202ebed4c1dc972e795b43f38bd21d
jakmate <jakub.orzolek04@gmail.com>
2025-05-16 23:51:41 +0100

    expense tracker added

--- app/static/js/activeTab.js
+++ app/static/js/activeTab.js
@@ -4,20 +4,22 @@ document.addEventListener("DOMContentLoaded", function() {
 
     window.addEventListener("scroll", () => {
         let current = "";
+        const navbarHeight = document.querySelector('.navbar').offsetHeight;
 
         sections.forEach(section => {
-            const sectionTop = section.offsetTop;
+            const sectionTop = section.offsetTop - navbarHeight;
             const sectionHeight = section.clientHeight;
-            if (pageYOffset >= sectionTop - 60 && pageYOffset < sectionTop + sectionHeight - 60) {
+            if (pageYOffset >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
                 current = section.getAttribute("id");
             }
         });
 
         navLinks.forEach(navLink => {
-            navLink.classList.remove("active");
+            const navBox = navLink.closest('.nav-box');
+            navBox.classList.remove("active");
             if (navLink.getAttribute("href").includes(current)) {
-                navLink.classList.add("active");
+                navBox.classList.add("active");
             }
-        })
+        });
     });
 });
